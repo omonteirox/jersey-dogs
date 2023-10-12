a9cde7ba340337f177ce95860fa46327db225df5
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-10-12 19:23:10 -0300

    Arrumando Utils

--- src/main/java/com/jersey_crud_api/utils/NotificationUtils.java
+++ src/main/java/com/jersey_crud_api/utils/NotificationUtils.java
@@ -3,7 +3,10 @@ package com.jersey_crud_api.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public abstract class NotificationUtils {
+	@JsonIgnore
 	 private List<String> notifications = new ArrayList<>();
 
 	    public List<String> getNotifications() {
