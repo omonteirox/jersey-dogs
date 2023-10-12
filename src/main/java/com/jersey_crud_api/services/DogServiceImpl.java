2a9f3213161d7b96d39ea5f1c187a307fcab3c69
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-10-12 19:23:05 -0300

    Arrumando Services

--- src/main/java/com/jersey_crud_api/services/DogServiceImpl.java
+++ src/main/java/com/jersey_crud_api/services/DogServiceImpl.java
@@ -8,153 +8,159 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import com.jersey_crud_api.dao.DogDAO;
 import com.jersey_crud_api.database.DbContext;
-import com.jersey_crud_api.exceptions.DbException;
 import com.jersey_crud_api.models.Dog;
-import com.jersey_crud_api.models.Tutor;
 
-public class TutorService {
+public class DogServiceImpl implements DogDAO{
 	private Connection connection;
-	private PreparedStatement preparedStatement = null;
-	private ResultSet resultSet = null;
-	public Tutor add(Tutor tutor) throws Exception {
+	public DogServiceImpl(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public Dog add(Dog dog)  {
+		connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		try {
 			connection = DbContext.Connect();
-			String query = "insert into tutor (name) values (?)";
+			String query = "insert into dog (name,birth_date,race,color,tutor_id) values (?,?,?,?,?)";
 			preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			preparedStatement.setString(1, tutor.getName());
+			preparedStatement.setString(1, dog.getName());
+			preparedStatement.setDate(2, dog.getBirthDate());
+			preparedStatement.setString(3, dog.getRace());
+			preparedStatement.setString(4, dog.getColor());
+			preparedStatement.setInt(5, dog.getTutorId());
 			preparedStatement.executeUpdate();
 			resultSet = preparedStatement.getGeneratedKeys();
 			if (resultSet.next()) {
-				tutor.setId(resultSet.getInt("id"));
+				dog.setId(resultSet.getInt("id"));
 			}
-			return tutor;
-			
-		}
-		catch(SQLException e) {
-			throw new DbException(e.getMessage(), e.getCause());
+			return dog;
+		} catch(SQLException e) {
 		}
 		catch (Exception e) {
-			throw new Exception(e.getMessage(), e.getCause());
-			}
+		}
 		finally {
 			DbContext.ClosePreparedStatement(preparedStatement);
 			DbContext.CloseResultSet(resultSet);
 			DbContext.Disconnect(connection);
 		}
+		return null;
 	}
-	public List<Tutor> getAll() throws Exception{
-		List<Tutor> tutorList = new ArrayList<Tutor>();
-		String query = "select * from tutor";
-		Tutor tutor = new Tutor();
+	public List<Dog> getAll() {
+		connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		List<Dog> dogList = new ArrayList<Dog>();
 		try {
 			connection = DbContext.Connect();
+			String query = "select id,name,birth_date,race,color,tutor_id from dog";
 			preparedStatement = connection.prepareStatement(query);
 			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
-				tutor = new Tutor();
-				tutor.setId(resultSet.getInt(1));
-				tutor.setName(resultSet.getString(2));
-				tutor.setDogs(getDogsOfTutor(tutor.getId()));
-				tutorList.add(tutor);
-			}	
-			
-			return tutorList;
-		}catch(SQLException e) {
-			throw new DbException(e.getMessage(), e.getCause());
+				Dog dog = new Dog();
+				dog.setId(resultSet.getInt("id"));
+				dog.setName(resultSet.getString("name"));
+				dog.setBirthDate(resultSet.getDate("birth_date"));
+				dog.setRace(resultSet.getString("race"));
+				dog.setColor(resultSet.getString("color"));
+				dog.setTutorId(resultSet.getInt("tutor_id"));
+				dogList.add(dog);
+			}
+			return dogList;
+		} catch(SQLException e) {
 		}
 		catch (Exception e) {
-			throw new Exception(e.getMessage(), e.getCause());
 		}finally {
 			DbContext.ClosePreparedStatement(preparedStatement);
 			DbContext.CloseResultSet(resultSet);
 			DbContext.Disconnect(connection);
 		}
-		
+		return null;
 	}
-	
-	public Tutor getById(Integer id) throws Exception {
-		String query = "select * from tutor where id = ?";
-		Tutor tutor = new Tutor();
+	public Dog getById(Integer id)  {
+		connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "select id,name,birth_date,race,color from dog where id = ?";
+		Dog dog = new Dog();
 		try {
-			List<Dog> dogs = getDogsOfTutor(id);
 			connection = DbContext.Connect();
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, id);
 			resultSet = preparedStatement.executeQuery();
 			if (resultSet.next()) {
-				tutor.setId(resultSet.getInt(1));
-				tutor.setName(resultSet.getString(2));
-				tutor.setDogs(dogs);
-				System.out.println(tutor.toString());
+				dog.setId(resultSet.getInt("id"));
+				dog.setName(resultSet.getString("name"));
+				dog.setBirthDate(resultSet.getDate("birth_date"));
+				dog.setRace(resultSet.getString("race"));
+				dog.setRace(resultSet.getString("color"));
 			}
-			return tutor;
-		}catch(SQLException e) {
-			throw new DbException(e.getMessage(), e.getCause());
+			return dog;
+		} catch(SQLException e) {
 		}
 		catch (Exception e) {
-			throw new Exception(e.getMessage(), e.getCause());
 		}finally {
 			DbContext.ClosePreparedStatement(preparedStatement);
 			DbContext.CloseResultSet(resultSet);
 			DbContext.Disconnect(connection);
 		}
+		return null;
 	}
-	public boolean delete(Integer id) throws Exception {
-		String query = "delete from tutor where id = ?";
+	public boolean delete(Integer id)  {
+		connection = null;
+		PreparedStatement preparedStatement = null;
+		String query = "delete from dog where id = ?";
 		try {
 			connection = DbContext.Connect();
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, id);
 			int rowsAffected = preparedStatement.executeUpdate();
 			if (rowsAffected == 0) {
+				System.err.println("Erro ao deletar cachorros -> ");
 				return false;
 			}
 			return true;
 		} catch(SQLException e) {
-			throw new DbException(e.getMessage(), e.getCause());
 		}
 		catch (Exception e) {
-			throw new Exception(e.getMessage(), e.getCause());
-			}finally {
+		}finally {
 			DbContext.ClosePreparedStatement(preparedStatement);
 			DbContext.Disconnect(connection);
 		}
+		return false;
 	}
-	public List<Dog> getDogsOfTutor(Integer id) throws Exception{
-		Connection connection = null;
+	public List<Dog> getByTutorId(Integer id)  {
+		connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
+		String query = "select id,name,birth_date,race,color,tutor_id from dog where tutor_id = ?";
 		List<Dog> dogs = new ArrayList<Dog>();
-		String query = "select * from dog where tutor_id = ?";
 		try {
 			connection = DbContext.Connect();
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, id);
 			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
+			while(resultSet.next()) {
 				Dog dog = new Dog();
 				dog.setId(resultSet.getInt("id"));
-				dog.setBirthDate(resultSet.getDate("birth_date"));
 				dog.setName(resultSet.getString("name"));
+				dog.setBirthDate(resultSet.getDate("birth_date"));
 				dog.setRace(resultSet.getString("race"));
 				dog.setColor(resultSet.getString("color"));
 				dog.setTutorId(resultSet.getInt("tutor_id"));
 				dogs.add(dog);
 			}
+			return dogs;
 		} catch(SQLException e) {
-			throw new DbException(e.getMessage(), e.getCause());
 		}
 		catch (Exception e) {
-			throw new Exception(e.getMessage(), e.getCause());
-			}
-		finally {
-			DbContext.Disconnect(connection);
+		}finally {
 			DbContext.ClosePreparedStatement(preparedStatement);
 			DbContext.CloseResultSet(resultSet);
+			DbContext.Disconnect(connection);
 		}
-		return dogs;
+		return null;
 	}
-	
 }
